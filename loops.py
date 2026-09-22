3a00be0d91d1dac4b71a380732fb7546dd99026d
shahriarrahman027 <shuvorahman932@gmail.com>
2026-09-22 13:51:34 +0600

    Add while_loops.py for odd number printing using while loop

---
+++ loops.py
@@ -0,0 +1,16 @@
+
+for i in range(1,6):
+    print(i)
+print(" ")
+for num in range(1,21):
+    if num%2==0:
+        print(num)
+print(" ")
+
+for num2 in range (11,0,-1):
+    print(num2)
+print(" ")
+
+number = int (input("Enter a number: "))
+for i in range (1,11):
+    print(number,"*",i,"=",number*i)
