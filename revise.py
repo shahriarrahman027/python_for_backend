48ed5404093ea5c9741639a866d3533a4b071801
shahriarrahman027 <shuvorahman932@gmail.com>
2026-09-22 11:48:54 +0600

    Enhance user interaction by adding age-related calculations and conditional statements in revise.py

--- revise.py
+++ revise.py
@@ -1,3 +1,27 @@
 name=input("Enter Your Name: ")
 print(name)
 print("Wellcome to SynitySoft")
+print(" ")
+
+print(" ")
+print("printing the complex version of the age:")
+age=20.2j
+print("your age is", age,"imagineri part is ",age.imag,"real part is",age.real)
+
+print("S" in name )
+print("X" in name )
+print("Y" not in name)
+
+print("Conditional Statements")
+print(" ")
+
+age=int(input("enter your age:"))
+
+print("your age is "+str(age))
+print(" ")
+if age>=18 and age<=40:
+    print("Your are eligible for the job")
+elif age>40 or age<18:
+    print("Sorry")
+else:
+    print("Next time")
